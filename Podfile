dbc4988bbc4279bf5cd5a02d1b4fcdf9ff4f3434
manytime777 <manytime@777.so-net.jp>
2016-07-08 17:10:10 +0900

    1st Commit.

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'p20160708' do
+	pod 'SwiftyDropbox'
+end
--- p20160708/ViewController.swift
+++ p20160708/ViewController.swift
@@ -7,12 +7,12 @@
 //
 
 import UIKit
+import SwiftyDropbox
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func linkButtonPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func signOutDropbox(sender: AnyObject) {
+    }
 
 }
 
